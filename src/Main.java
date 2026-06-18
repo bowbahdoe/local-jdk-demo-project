c8d5fb877df418cd2fd333b2a03d76ecc48c255d
Ethan McCue <ethan@mccue.dev>
2026-06-18 09:47:18 -0400

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import dev.mccue.magicbean.MagicBean;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import module javafx.controls;
 
 public class Main extends Application {
     public static void main(String[] args) {
